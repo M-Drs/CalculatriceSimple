e4026b98f366bd72fb289506adb02c4efa485562
Gwhite <guilhem.dolet@gmail.com>
2024-04-18 09:40:52 +0200

    Feature addition

--- Source/addition.py
+++ Source/addition.py
@@ -1,5 +1,7 @@
-# Guilhem travail sur la fonction addition:
+# Guilhem travail sur la fonction addition
+# from colorama import Fore, Style : pour faire des couleurs f"{Fore.GREEN}RESULTAT:{Style.RESET_ALL}"
+
 
 def addition(nombre1: float, nombre2: float):
-    return nombre1 + nombre2
+    return print(f"RESULTAT: {nombre1 + nombre2}")
 
