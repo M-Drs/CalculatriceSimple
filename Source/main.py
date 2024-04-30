06d03baae878228993bf592ed886ed6869d3775d
DaiTensa <dai.tensaout@gmail.com>
2024-04-30 14:09:50 +0200

    création d'une fonction code source calculatrice pour test

--- Source/main.py
+++ Source/main.py
@@ -1,13 +1,9 @@
 # Import
-from colorama import Fore, Style
 import sys
-from division import division_func
-from multiplication import multiplication_func
-from addition import addition
-from soustraction import soustraction
 from fireworks import fireworks
 import emoji
 from dotenv import load_dotenv
+from code_source_calculatrice import run_calculatrice
 import os
 load_dotenv()
 
@@ -36,54 +32,5 @@ choix_possible = ["1", "2", "3", "4", "q"]
 choix_utilisateur = None
 continuer = "o"
 
-
-# Gestion des erreurs
-# division par 0 ? 
-# Si entrée invalide -> Message
-
-
-
-while continuer == "o":
-
-  
-
-    choix_utilisateur = input(menu)
-
-    if choix_utilisateur not in choix_possible:
-        print(f"{Fore.RED}Veuillez choisir une option valide...{Style.RESET_ALL}")
-        continue
-
-    else:
-
-        if choix_utilisateur == "q":
-                sys.exit()
-
-        premier_nombre = input(phrase_premier_nombre)
-        deuxieme_nombre = input(phrase_deuxieme_nombre)
-
-        if  premier_nombre.isdigit() and deuxieme_nombre.isdigit():
-            
-
-            if choix_utilisateur == "1":
-                # Code addition
-                addition_result = addition(premier_nombre, deuxieme_nombre)
-                print(f"{Fore.GREEN}{premier_nombre} + {deuxieme_nombre} = {addition_result}{Style.RESET_ALL}")
-
-            elif choix_utilisateur == "2":
-                # Code soustraction
-                soustraction_result = soustraction(premier_nombre, deuxieme_nombre)
-                print(f"{Fore.GREEN}{premier_nombre} - {deuxieme_nombre} = {soustraction_result}{Style.RESET_ALL}")
-                
-
-            elif choix_utilisateur == "3":
-                # Code Multiplication
-                multiplication = multiplication_func(premier_nombre=premier_nombre, deuxieme_nombre=deuxieme_nombre)
-                print(f"{Fore.GREEN}{premier_nombre} x {deuxieme_nombre} = {multiplication}{Style.RESET_ALL}")
-
-            elif choix_utilisateur == "4":
-                # Code Division
-                division = division_func(premier_nombre=premier_nombre, deuxieme_nombre=deuxieme_nombre)
-                print(f"{Fore.GREEN}{premier_nombre} / {deuxieme_nombre} = {division}{Style.RESET_ALL}")
-        
-        else:
-            print(f"{Fore.RED}Entrée non valide !{Style.RESET_ALL}")
+if __name__ == "__main__":
+    run_calculatrice(menu, choix_possible, phrase_premier_nombre, phrase_deuxieme_nombre)
