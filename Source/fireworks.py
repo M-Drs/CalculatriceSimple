491459cacb0d1eeeabb55a9cb9ff7c2be59f075f
Gwhite <guilhem.dolet@gmail.com>
2024-04-30 11:59:14 +0200

    création d'un fonction fireworks

---
+++ Source/fireworks.py
@@ -0,0 +1,27 @@
+import emoji
+from colorama import Fore, Style
+def fireworks(accord):
+    
+    l1=r"                                   .''.       "
+    l2=r"       .''.      .        *''*    :_\/_:     . "
+    l3=r"      :_\/_:   _\(/_  .:.*_\/_*   : /\ :  .'.:.'."
+    l4=r"  .''.: /\ :   ./)\   ':'* /\ * :  '..'.  -=:o:=-"
+    l5=r" :_\/_:'.:::.    ' *''*    * '.\'/.' _\(/_'.':'."
+    l6=r" : /\ : :::::     *_\/_*     -= o =-  /)\    '  *"
+    l7=r"  '..'  ':::'     * /\ *     .'/.\'.   "
+    l8=r"      *            *..*         :"
+
+    if accord == True: 
+        return     
+    
+    emoji.emojize(f"':party_popper::party_popper::party_popper: {Fore.RED} WELCOME {Style.RESET_ALL} :party_popper::party_popper::party_popper:'"))
+    f"{Fore.GREEN}{l1}{Style.RESET_ALL}")
+    f"{Fore.GREEN}{l2}{Style.RESET_ALL}")
+    f"{Fore.YELLOW}{l3}{Style.RESET_ALL}")
+    f"{Fore.YELLOW}{l4}{Style.RESET_ALL}")
+    f"{Fore.GREEN}{l5}{Style.RESET_ALL}")
+    f"{Fore.CYAN}{l6}{Style.RESET_ALL}")
+    f"{Fore.CYAN}{l7}{Style.RESET_ALL}")
+    f"{Fore.MAGENTA}{l8}{Style.RESET_ALL}")
+
+
